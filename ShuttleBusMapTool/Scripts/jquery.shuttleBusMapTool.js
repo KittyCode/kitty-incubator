3c68677bc11e03831f6afb22b5bc4edd0a851da2
Kitty Wang <kitty2.wang@sonymobile.com>
2013-01-17 15:18:36 +0800

    Update

--- ShuttleBusMapTool/Scripts/jquery.shuttleBusMapTool.js
+++ ShuttleBusMapTool/Scripts/jquery.shuttleBusMapTool.js
@@ -1,7 +1,9 @@
 ﻿(function ($) {
 
-    $.fn.hoverElement = function () {
-        this.each(function () {
+    //----------- This is only a demo for jQuery plugin --------------//
+
+    $.fn.hoverElement = function () {   //对象(实例)方法附加到jQuery.fn对象上
+        return this.each(function () {     //return: 返回jQuery对象以实现链式操作
             $(this).hover(
                 function () {
                     $(this).addClass("hoverStyle");
@@ -13,6 +15,10 @@
                 }
             );
         });
-    }
+    };
+
+    //----------------- Shuttle Bus Map Tool -------------------//
+
+    // Coming Soon...
 
 })(jQuery); 
