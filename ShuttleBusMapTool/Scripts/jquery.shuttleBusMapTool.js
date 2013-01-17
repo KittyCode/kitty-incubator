5693e8e4f6f9817e53a1b5790a51d9fe7047dc1d
Kitty Wang <kitty2.wang@sonymobile.com>
2013-01-17 11:28:21 +0800

    initial commit

---
+++ ShuttleBusMapTool/Scripts/jquery.shuttleBusMapTool.js
@@ -0,0 +1,18 @@
+﻿(function ($) {
+
+    $.fn.hoverElement = function () {
+        this.each(function () {
+            $(this).hover(
+                function () {
+                    $(this).addClass("hoverStyle");
+                }
+            );
+            $(this).mouseout(
+                function () {
+                    $(this).removeClass("hoverStyle");
+                }
+            );
+        });
+    }
+
+})(jQuery); 
