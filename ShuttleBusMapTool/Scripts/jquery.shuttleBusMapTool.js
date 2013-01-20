7df4cf978c521e04f3a3ad3da9ccbfdad67b21dd
robin521111 <luhaibin731@sina.com>
2013-01-21 00:22:43 +0800

    add namespace for template
    add namespace for sample. now we could add more function into
    ShuttleBusMapTool in this way ShuttleBusMap('Method name'). tested and
    works fine.

--- ShuttleBusMapTool/Scripts/jquery.shuttleBusMapTool.js
+++ ShuttleBusMapTool/Scripts/jquery.shuttleBusMapTool.js
@@ -1,6 +1,50 @@
-﻿(function ($) {
+﻿// i'd like add some comments here , what i referenced from jquery.com
+//(function($){}(jqury)) is to keep "dollar sign" works
+
+
+(function ($) {
 
     //----------- This is only a demo for jQuery plugin --------------//
+
+
+
+    var methods={
+
+        init: function () {
+            return this.each(function () {
+                $(this).hover(
+                    function () {
+                        $(this).addClass("hoverStyle");
+                    }
+                );
+                $(this).mouseout(
+                    function () {
+                        $(this).removeClass("hoverStyle");
+                    });
+            });
+        },
+
+        maxMapHeight:function () {
+            var max;
+            return this.each(function () {
+                max=Math.max(max,$(this).height());
+            });
+        }
+
+
+    };
+
+$.fn.shuttleBusMapTool=function (method) {
+    if (methods[method]) {
+        return methods[method].apply(this,Array.prototype.slice.call(arguments,1));
+
+    }else if (typeof method==='object'|| method ) {
+        return methods.init.apply(this,arguments);
+    }else{
+        $.error('Method'+method +'does not exist on jquery.shuttleBusMapTool');
+    
+}
+};
 
     $.fn.hoverElement = function () {   //对象(实例)方法附加到jQuery.fn对象上
         return this.each(function () {     //return: 返回jQuery对象以实现链式操作
@@ -17,8 +61,29 @@
         });
     };
 
+    // here comes some prototype design
+    $.fn.maxMapHeight=function () {
+        //body...
+        var max =0;
+        this.each(function  () {
+            max=Math.max(max, $(this).height());
+
+        });
+        return max;
+
+    };
+
+
     //----------------- Shuttle Bus Map Tool -------------------//
 
     // Coming Soon...
 
 })(jQuery); 
+
+
+
+//if you don't need doller sign, you can add you plugin in this way
+
+
+
+
